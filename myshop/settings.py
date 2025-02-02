38c09822f1ceb76fc4b3a2c0db5f5c8744141ab8
Rui-Yamaguchi <rui.yamaguchi1122@gmail.com>
2025-02-02 14:56:48 +0900

    DEBUG

--- myshop/settings.py
+++ myshop/settings.py
@@ -27,8 +27,7 @@ env.read_env(os.path.join(BASE_DIR, '.env'))
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = env.bool('DEBUG')
-DEBUG = True
+DEBUG = env.bool('DEBUG')
 
 ALLOWED_HOSTS = ['127.0.0.1']
 
